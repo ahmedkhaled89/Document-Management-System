20d8c9a6eb3ad775a99aa56e792ce77477a029c0
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-08-20 19:57:45 +0300

    Add User Model

---
+++ models/UserModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  nationalID: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  token: {
+    type: String,
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
