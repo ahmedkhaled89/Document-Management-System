5c1a069fa5faa0cfaf3d9ab1a16f1dd0064357d6
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-08-26 01:08:53 +0300

    update Docs Model

--- models/DocumentModel.js
+++ models/DocumentModel.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const DocSchema = new Schema({
-  name: { type: String, required: true, default: 'TEST Doc' },
+  docName: { type: String, required: true, default: 'TEST Doc' },
   docType: { type: String, required: true, default: 'pdf' },
   ownerID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  destination: { type: String, required: true },
   workspaceID: {
     type: Schema.Types.ObjectId,
     ref: 'Workspace',
   },
+  docPath: { type: String },
+  originalname: { type: String },
 });
 const Doc = mongoose.model('Doc', DocSchema);
 module.exports = Doc;
