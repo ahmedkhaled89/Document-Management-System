441d823605ed79ea783cbda44dbad476ad27b30b
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-04 13:36:06 +0300

    Add schemaCheck middleware for register endpoint

--- routers/UserRoutes.js
+++ routers/UserRoutes.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
+const { checkSchema } = require('express-validator');
 
 const UserRouter = express.Router();
 
-UserRouter.post('/api/users/register', UserController.register);
+UserRouter.post(
+  '/api/users/register',
+  checkSchema(
+    {
+      email: { isEmail: true },
+      nationalID: { notEmpty: true },
+      firstName: { notEmpty: true },
+      lastName: { notEmpty: true },
+      password: { notEmpty: true, isLength: { options: { min: 8 } } },
+    },
+    ['body']
+  ),
+  UserController.register
+);
 UserRouter.post('/api/users/login', UserController.login);
 
 module.exports = UserRouter;
