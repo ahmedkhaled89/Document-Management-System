0247345c5a04be40ef96ad36e2261beea4ffcc8d
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-12 20:14:41 +0300

    Add time stamp to document

--- models/DocumentModel.js
+++ models/DocumentModel.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const DocSchema = new Schema({
-  docName: { type: String, required: true, default: 'TEST Doc' },
-  docType: { type: String, required: true, default: 'pdf' },
-  ownerID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  destination: { type: String, required: true },
-  workspaceID: {
-    type: Schema.Types.ObjectId,
-    ref: 'Workspace',
+const DocSchema = new Schema(
+  {
+    docName: { type: String, required: true, default: 'TEST Doc' },
+    docType: { type: String, required: true, default: 'pdf' },
+    ownerID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    destination: { type: String, required: true },
+    workspaceID: {
+      type: Schema.Types.ObjectId,
+      ref: 'Workspace',
+    },
+    docPath: { type: String },
+    originalname: { type: String },
+    deleted: { type: Boolean, default: false },
   },
-  docPath: { type: String },
-  originalname: { type: String },
-  deleted: { type: Boolean, default: false },
-});
+  { timestamps: true }
+);
 const Doc = mongoose.model('Doc', DocSchema);
 module.exports = Doc;
