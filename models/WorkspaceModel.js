816dfdacd78dbb5aa79569b343d196281da95835
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-23 15:55:13 +0300

    Add timestamps to Workspace Schema

--- models/WorkspaceModel.js
+++ models/WorkspaceModel.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-const WorkspaceSchema = new Schema({
-  name: { type: String, required: true },
-  ownerID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  DocsIDs: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Doc',
-    },
-  ],
-  deleted: { type: Boolean, default: false },
-});
+const WorkspaceSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    ownerID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    DocsIDs: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Doc',
+      },
+    ],
+    deleted: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 WorkspaceSchema.index({ ownerID: 1, name: 1 }, { unique: true });
 
